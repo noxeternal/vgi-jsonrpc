27ae9b601824a9c2642dd226028da4c55c7fc717
Michael Rice <rice.michaelt@gmail.com>
2019-06-20 17:03:47 -0500

    daily commit

---
+++ inc/colorStreamHandler.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace Monolog\Handler;
+use Monolog\Logger;
+
+class colorStreamHandler extends StreamHandler {
+  // private $keywords = [
+  //   'SELECT',
+  //   'INSERT'
+  // ];
+
+  // private $colorMap = [
+  //   'black',
+  //   'blue',
+  //   'green',
+  //   'cyan',
+  //   'red',
+  //   'purple',
+  //   'brown',
+  //   'lightGray',
+  //   'darkGray',
+  //   'lightBlue',
+  //   'lightGreen',
+  //   'lightCyan',
+  //   'lightRed',
+  //   'lightPurple',
+  //   'yellow',
+  //   'white',
+  // ];
+  /**
+   * Write to stream
+   * @param resource $stream
+   * @param array    $record
+   */
+  protected function streamWrite($stream, array $record): void{
+    // foreach ($colorMap as $color => $keywords) {
+    //   $record['formatted'] = $this->replace($record['formatted'], $color, $keywords);
+    // }
+
+    $color = false;
+    if(strpos($record['formatted'], 'INSERT') > 0)
+      $color = 'green';
+    if(!$color && strpos($record['formatted'], 'INFO') > 0)
+      $color = 'blue';
+    $color = $color?$color:'red';
+      
+    fwrite($stream, $color((string) $record['formatted']));
+  }
+
+  // private function replace($f, $r) {
+
+  // }
+}
---
+++ inc/colors.php
@@ -0,0 +1,65 @@
+<?php
+
+function black ($s) {
+  return "\033[30m {$s} \033[0m";
+} 
+
+function blue ($s) {
+  return "\033[34m {$s} \033[0m";
+} 
+
+function green ($s) {
+  return "\033[32m {$s} \033[0m";
+} 
+
+function cyan ($s) {
+  return "\033[36m {$s} \033[0m";
+} 
+
+function red ($s) {
+  return "\033[31m {$s} \033[0m";
+} 
+
+function purple ($s) {
+  return "\033[35m {$s} \033[0m";
+} 
+
+function brown ($s) {
+  return "\033[33m {$s} \033[0m";
+} 
+
+function lightGray ($s) {
+  return "0\033[7 m {$s} \033[0m";
+} 
+
+function darkGray ($s) {
+  return "\033[30m {$s} \033[0m";
+} 
+
+function lightBlue ($s) {
+  return "\033[34m {$s} \033[0m";
+} 
+
+function lightGreen ($s) {
+  return "\033[32m {$s} \033[0m";
+} 
+
+function lightCyan ($s) {
+  return "\033[36m {$s} \033[0m";
+} 
+
+function lightRed ($s) {
+  return "\033[31m {$s} \033[0m";
+} 
+
+function lightPurple ($s) {
+  return "\033[35m {$s} \033[0m";
+} 
+
+function yellow ($s) {
+  return "\033[33m {$s} \033[0m";
+} 
+
+function white ($s) {
+  return "\033[37m {$s} \033[0m";
+}
